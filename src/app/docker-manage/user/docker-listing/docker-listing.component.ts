e3f09c7c4f5c96b2f07aed018614919a7f36117c
pc7 <lynnhtetaung4@gmail.com>
2024-01-15 19:47:23 +0900

    added get image name api and

--- src/app/docker-manage/user/docker-listing/docker-listing.component.ts
+++ src/app/docker-manage/user/docker-listing/docker-listing.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8572f593aa4229f53af1056c21a70e801e65408ada49f11307625c21e1ea06c0
-size 6020
+oid sha256:045344e3570064366ebaf9c153d3adee5e9dc481e806cfe840b72ff96246a76e
+size 5978
